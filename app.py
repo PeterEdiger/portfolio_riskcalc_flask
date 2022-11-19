ee95363d842cebc28454d57106476fe10cc0cda7
peet <teilzeitjobbewerber@gmx.de>
2022-11-19 14:07:37 +0100

    calculations improved

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def risk_calc():
 @app.route('/calc_result', methods=["POST", "GET"])
 def calc_result():
     kurs_str = request.args.get("kurs")
-    stop_dollar_str = request.args.get("stop_loss_dollar")
-    stop_prozent_str = request.args.get("stop_loss_prozent")
+    selling_threshold_loss_dollar_str = request.args.get("selling_threshold_loss_dollar")
+    loss_prct_str = request.args.get("loss_prct")
 
     if kurs_str == "":
         # Fall 1
@@ -31,25 +31,25 @@ def calc_result():
 
         # Fall 2 bis 5
     kurs_int = int(kurs_str)
-    if stop_dollar_str == "" and stop_prozent_str == "":
+    if selling_threshold_loss_dollar_str == "" and loss_prct_str == "":
         # Fall 2
         return render_template("calc_result.html", user_warning="Bitte Stop-Loss $"
                                                                 "oder Stop-Loss % angeben")
-    if stop_dollar_str == "" and stop_prozent_str:
+    if not selling_threshold_loss_dollar_str and loss_prct_str:
         # Fall 3
-        dollar_int = kurs_int * (100 - int(stop_prozent_str)) / 100
-        return render_template("calc_result.html", kurs=kurs_int, stop_dollar=dollar_int,
-                               verlust_dollar=kurs_int - dollar_int)
+        selling_threshold_loss_dollar_int = kurs_int * (100 - int(loss_prct_str)) // 100
+        return render_template("calc_result.html", kurs=kurs_int, stop_dollar=selling_threshold_loss_dollar_int,
+                               verlust_dollar=kurs_int - selling_threshold_loss_dollar_int)
+    if selling_threshold_loss_dollar_str and not loss_prct_str:
+        # Fall 4
+        selling_threshold_loss_dollar_int = int(selling_threshold_loss_dollar_str)
 
-    return render_template("calc_result.html", user_warning = "Fall 4, 5")
+        return render_template("calc_result.html", kurs=kurs_int, stop_dollar=selling_threshold_loss_dollar_int,
+                               verlust_dollar=kurs_int - selling_threshold_loss_dollar_int)
 
-    # stop_prozent_int = 100 * stop_dollar_int // kurs_int
+    # Fall 5
 
-    # profit_dollar = int(request.args.get("take_profit_dollar"))
-    # profit_prozent = int(request.args.get("take_profit_prozent"))
-    # verlust_dollar, verlust_prozent = loss_dollar_calc(kurs_int, stop_dollar_int)
-    # stop_prozent_int = float(int(stop_loss_prozent))
-    # verlust_in_prozent = 100 * (kurs_int - stop_dollar_flt) / kurs_int
+    return render_template("calc_result.html", user_warning="Abverkaufskurs oder Verlust in Prozent eintragen bitte.")
 
 
 if __name__ == '__main__':
@@ -57,11 +57,9 @@ if __name__ == '__main__':
 
 # Todos
 
+# todo Logik auf Gewinn Möglichkeiten ausweiten. Planung aller Möglichkeiten
+# todo Weitere Parameter wie Stückzahl Maximaler Verlust in die Rechnungen einbeziehen.
 # todo Die Html forms sollen ausgefüllt bleiben, (vertagt)
 #  Html forms sollen wenn es Sinn macht automatisch ausgefüllt werden (vertagt)
-# todo Zuendeführen der Stop-Loss Fälle / Variablen bennenen nach threshold und difference
-# todo requirements für Papa und Einladung
-# todo leere Felder der HTML-Form zulassen (Für Losses erledigt)
-#  Was soll bei Den Möglichen Fällen für Stop-Loss passieren z.B Stop-Dollar gefüllt, Stop-Prozent leer.
+
 # todo Ausgabe: Verlust in $ und %, Gewinn in $ und %
-# todo Was sind decorators in Python https://www.programiz.com/python-programming/decorator
