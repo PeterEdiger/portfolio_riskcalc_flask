ff799c7d008fbcea38699e6f12fbde2af6eecb38
peet <teilzeitjobbewerber@gmx.de>
2022-11-23 22:43:02 +0100

    Loss calculations finished

--- app.py
+++ app.py
@@ -21,33 +21,61 @@ def risk_calc():
 
 @app.route('/calc_result', methods=["POST", "GET"])
 def calc_result():
+    selling_threshold_win_dollar_str = request.args.get("selling_threshold_win_dollar")
+    win_prct_str = request.args.get("win_prct")
+
+    # --------------------------------------------------------------------------------#
+    # Stückzahl einlesen default Wert = 1
+    quantity = request.args.get("quantity")
+    if quantity:
+        quantity = int(quantity)
+    else:
+        quantity = 1
+    # --------------------------------------------------------------------------------#
+    # Kurs einlesen, wenn nicht vorhanden --> user_message
     kurs_str = request.args.get("kurs")
+    if kurs_str == "":
+        # No price input case
+        return render_template("calc_result.html", user_warning="Bitte Kurs eingeben")
+    else:
+        kurs_int = int(kurs_str)
+
+    # --------------------------------------------------------------------------------#
+    total_invest = kurs_int * quantity
+
+    # --------------------------------------------------------------------------------#
+    # Stop-Loss %, Stop-Loss $ einlesen / Wenn beide vorhanden -> user_message
+    # Stop-Loss $ berechnen.
+
     selling_threshold_loss_dollar_str = request.args.get("selling_threshold_loss_dollar")
     loss_prct_str = request.args.get("loss_prct")
+    # Abkürzungen
+    s_t_l_d_s = selling_threshold_loss_dollar_str
+    rt = render_template
+    # Fall beide vorhanden
+    if s_t_l_d_s and loss_prct_str:
+        return rt("calc_result.html", user_message="Bitte eine Größe eingeben.")
+    # nur Stop-Loss $
+    if s_t_l_d_s and not loss_prct_str:
+        s_t_l_d_i = int(s_t_l_d_s)
+        loss_prct_int = round((kurs_int - s_t_l_d_i) / kurs_int * 100)
 
-    if kurs_str == "":
-        # Fall 1
-        return render_template("calc_result.html", user_warning="Bitte Kurs eingeben")
+    # nur Stop-Loss %
+    if not s_t_l_d_s and loss_prct_str:
+        loss_prct_int = int(loss_prct_str)
+        s_t_l_d_i = kurs_int * ((100 - loss_prct_int) / 100)
 
-        # Fall 2 bis 5
-    kurs_int = int(kurs_str)
-    if selling_threshold_loss_dollar_str == "" and loss_prct_str == "":
-        # Fall 2
-        return render_template("calc_result.html", user_warning="Bitte Stop-Loss $"
-                                                                "oder Stop-Loss % angeben")
-    if not selling_threshold_loss_dollar_str and loss_prct_str:
-        # Fall 3
-        selling_threshold_loss_dollar_int = kurs_int * (100 - int(loss_prct_str)) // 100
-        return render_template("calc_result.html", kurs=kurs_int, stop_dollar=selling_threshold_loss_dollar_int,
-                               verlust_dollar=kurs_int - selling_threshold_loss_dollar_int)
-    if selling_threshold_loss_dollar_str and not loss_prct_str:
-        # Fall 4
-        selling_threshold_loss_dollar_int = int(selling_threshold_loss_dollar_str)
-
-        return render_template("calc_result.html", kurs=kurs_int, stop_dollar=selling_threshold_loss_dollar_int,
-                               verlust_dollar=kurs_int - selling_threshold_loss_dollar_int)
-
-    # Fall 5
+    if not s_t_l_d_s and not loss_prct_str:
+        s_t_l_d_i = None
+        loss_dollar = None
+        total_loss = None
+        loss_prct_int = None
+    # Weil einer der oberen Fälle = True
+    else:
+        total_loss = quantity * (kurs_int - s_t_l_d_i)
+        loss_dollar = kurs_int -s_t_l_d_i
+    return rt("calc_result.html", kurs=kurs_int, selling_threshold_loss_dollar=s_t_l_d_i, total_invest=total_invest,
+              loss_dollar=loss_dollar, total_loss=total_loss, loss_prct=loss_prct_int)
 
     return render_template("calc_result.html", user_warning="Abverkaufskurs oder Verlust in Prozent eintragen bitte.")
 
@@ -57,9 +85,18 @@ if __name__ == '__main__':
 
 # Todos
 
+# Ideen
+"""
+Zur Übersicht Funktion bauen: 
+1. Berechnet alle gültigen Fälle
+2. Kümmert sich um die ungültigen Fälle
+
+Optische Aufteilung der Suchfenster Gewinn rechts neben Verlust 
+Abkürzungen benutzen für die Übersicht?
+"""
 # todo Logik auf Gewinn Möglichkeiten ausweiten. Planung aller Möglichkeiten
-# todo Weitere Parameter wie Stückzahl Maximaler Verlust in die Rechnungen einbeziehen.
 # todo Die Html forms sollen ausgefüllt bleiben, (vertagt)
 #  Html forms sollen wenn es Sinn macht automatisch ausgefüllt werden (vertagt)
-
 # todo Ausgabe: Verlust in $ und %, Gewinn in $ und %
+
+
