0dbc3a8fe1a083f22adb6eb58204df8fe144ecf7
peet <teilzeitjobbewerber@gmx.de>
2022-12-02 11:30:53 +0100

    functioning calculations. Formatted code

--- app.py
+++ app.py
@@ -21,11 +21,8 @@ def risk_calc():
 
 @app.route('/calc_result', methods=["POST", "GET"])
 def calc_result():
-    selling_threshold_win_dollar_str = request.args.get("selling_threshold_win_dollar")
-    win_prct_str = request.args.get("win_prct")
-
-    # --------------------------------------------------------------------------------#
-    # Stückzahl einlesen default Wert = 1
+    # ------------------------Stückzahl einlesen default Wert = 1---------------------#
+    #
     quantity = request.args.get("quantity")
     if quantity:
         quantity = int(quantity)
@@ -39,45 +36,97 @@ def calc_result():
         return render_template("calc_result.html", user_warning="Bitte Kurs eingeben")
     else:
         kurs_int = int(kurs_str)
-
     # --------------------------------------------------------------------------------#
     total_invest = kurs_int * quantity
-
-    # --------------------------------------------------------------------------------#
-    # Stop-Loss %, Stop-Loss $ einlesen / Wenn beide vorhanden -> user_message
-    # Stop-Loss $ berechnen.
-
+    # -------------------------------Loss Fälle einlesen-------------------------------#
     selling_threshold_loss_dollar_str = request.args.get("selling_threshold_loss_dollar")
     loss_prct_str = request.args.get("loss_prct")
-    # Abkürzungen
+    # -------------------------------Win Fälle einlesen-------------------------------#
+    selling_threshold_win_dollar_str = request.args.get("selling_threshold_win_dollar")
+    win_prct_str = request.args.get("win_prct")
+    # --------------------------------Tolerierten Verlust einlesen--------------------------- #
+    tolerated_loss_str = request.args.get("tolerated_loss")
+
+    crv_str = request.args.get("crv")
+
+    # --------------------------------Abkürzungen--------------------------------------#
     s_t_l_d_s = selling_threshold_loss_dollar_str
+    s_t_w_d_s = selling_threshold_win_dollar_str
     rt = render_template
-    # Fall beide vorhanden
+    # -------------------------All variables which can be None set to None-------------- #
+    s_t_l_d_i = s_t_w_d_i = loss_per_stock = total_loss \
+        = loss_prct_int = win_prct_int = \
+        win_per_stock = total_win = crv_float = tolerated_loss_int = None
+
+    # ------------------------ Fall beide Loss Felder gefüllt----------------------------#
     if s_t_l_d_s and loss_prct_str:
-        return rt("calc_result.html", user_message="Bitte eine Größe eingeben.")
+        return rt("calc_result.html", user_warning="Bitte nur eine Größe eingeben.")
+    # ------------------------ Fall beide Win Felder gefüllt----------------------------#
+    if s_t_w_d_s and win_prct_str:
+        return rt("calc_result.html", user_warning="Bitte nur eine Größe eingeben")
+
     # nur Stop-Loss $
-    if s_t_l_d_s and not loss_prct_str:
+    if s_t_l_d_s:
         s_t_l_d_i = int(s_t_l_d_s)
         loss_prct_int = round((kurs_int - s_t_l_d_i) / kurs_int * 100)
 
     # nur Stop-Loss %
-    if not s_t_l_d_s and loss_prct_str:
+    if loss_prct_str:
         loss_prct_int = int(loss_prct_str)
         s_t_l_d_i = kurs_int * ((100 - loss_prct_int) / 100)
 
-    if not s_t_l_d_s and not loss_prct_str:
-        s_t_l_d_i = None
-        loss_dollar = None
-        total_loss = None
-        loss_prct_int = None
-    # Weil einer der oberen Fälle = True
-    else:
-        total_loss = quantity * (kurs_int - s_t_l_d_i)
-        loss_dollar = kurs_int -s_t_l_d_i
-    return rt("calc_result.html", kurs=kurs_int, selling_threshold_loss_dollar=s_t_l_d_i, total_invest=total_invest,
-              loss_dollar=loss_dollar, total_loss=total_loss, loss_prct=loss_prct_int)
+    if tolerated_loss_str:
+        tolerated_loss_int = int(tolerated_loss_str)
+        quantity = tolerated_loss_int / (kurs_int - s_t_l_d_i)
 
-    return render_template("calc_result.html", user_warning="Abverkaufskurs oder Verlust in Prozent eintragen bitte.")
+    # nur Take-Profit $
+    if s_t_w_d_s:
+        s_t_w_d_i = int(s_t_w_d_s)
+        win_prct_int = round((s_t_w_d_i - kurs_int) / kurs_int * 100)
+
+    # nur Take-Profit %
+    if win_prct_str:
+        win_prct_int = int(win_prct_str)
+        s_t_w_d_i = round(kurs_int * ((100 + win_prct_int) / 100))
+
+    if not s_t_l_d_i and not s_t_w_d_i:
+        return render_template("calc_result.html", user_warning="Mehr Angaben benötigt.")
+
+    if s_t_l_d_i:
+        loss_per_stock = kurs_int - s_t_l_d_i
+        total_loss = quantity * loss_per_stock
+
+    if s_t_w_d_i:
+        win_per_stock = s_t_w_d_i - kurs_int
+        total_win = quantity * win_per_stock
+
+    if crv_str:
+        crv_float = float(crv_str)
+
+    if s_t_l_d_i and s_t_w_d_i and crv_float:
+        return rt("calc_result.html", user_warning="Zu viele Angaben.")
+
+    if s_t_l_d_i and s_t_w_d_i:
+        crv_float = round(win_per_stock / loss_per_stock, 1)
+
+    # CRV and Loss
+    if s_t_l_d_i and crv_float and not s_t_w_d_i:
+        win_per_stock = round((kurs_int - s_t_l_d_i) * crv_float)
+        s_t_w_d_i = kurs_int + win_per_stock
+        win_prct_int = round(100 * win_per_stock / kurs_int)
+        total_win = quantity * win_per_stock
+
+    # CRV and Win
+    if s_t_w_d_i and crv_float and not s_t_l_d_i:
+        loss_per_stock = round((s_t_w_d_i - kurs_int) / crv_float)
+        s_t_l_d_i = kurs_int - loss_per_stock
+        loss_prct_int = round(loss_per_stock / kurs_int * 100)
+        total_loss = quantity * loss_per_stock
+
+    return rt("calc_result.html", kurs=kurs_int, total_invest=total_invest,
+              s_t_l_d=s_t_l_d_i, total_loss=total_loss, loss_prct=loss_prct_int, loss_per_stock=loss_per_stock,
+              s_t_w_d=s_t_w_d_i, total_win=total_win, win_prct=win_prct_int, win_per_stock=win_per_stock,
+              crv=crv_float, quantity=quantity, tolerated_loss=tolerated_loss_int)
 
 
 if __name__ == '__main__':
@@ -94,9 +143,13 @@ Zur Übersicht Funktion bauen:
 Optische Aufteilung der Suchfenster Gewinn rechts neben Verlust 
 Abkürzungen benutzen für die Übersicht?
 """
-# todo Logik auf Gewinn Möglichkeiten ausweiten. Planung aller Möglichkeiten
-# todo Die Html forms sollen ausgefüllt bleiben, (vertagt)
+
+# todo None Variablen sollen nicht auf der html Seite erscheinen
+
+# ----- Mögliche Verbesserungen -------#
+# Einfachsten Faller zwei Felder / beim manipulieren von einem Fenster wird das
+# andere aktualisiert.
+#  Die Html forms sollen ausgefüllt bleiben, (vertagt)
 #  Html forms sollen wenn es Sinn macht automatisch ausgefüllt werden (vertagt)
-# todo Ausgabe: Verlust in $ und %, Gewinn in $ und %
 
 
